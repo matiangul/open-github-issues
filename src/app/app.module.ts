c832d4befffb536921cd1027c019ce0c00ff0177
Mateusz Angulski <mateusz.angulski@assertis.co.uk>
2019-09-07 23:52:12 +0200

    Add dummy search input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchInputComponent } from './search-input/search-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchInputComponent
   ],
   imports: [
     BrowserModule
